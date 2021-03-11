fbed54167719216ac10f575f2ee53b799f57ef1e
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-03-10 23:25:25 -0600

    added local storage saving, created a function to retrive upon load, trying to recreate w/ .each, finalized function to color code hour blocks

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,5 @@
 var timeDisplayEl = $("#currentDay");
-
-// var timeBlockEl = $(".time-block");
-// var hourNine = $("#09");
-// var hourTen = $("#10");
-// var hourEleven = $("#11");
-// var hourTwelve = $("#12");
-// var hourThirteen = $("#13");
-// var hourFourteen = $("#14");
-// var hourFifteen = $("#15");
-// var hourSixteen = $("#16");
-// var hourSeventeen = $("#17");
+var saveButton = $(".saveBtn");
 
 // Display for current date/time @ header
 function displayDateAndTime() {
@@ -26,20 +16,56 @@ var currentHour = getCurrentHour();
 
 console.log(currentHour);
 
+
+
 // Function to color code each timeblock element
 $(".time-block").each(function() {
     var hourDivEl = $(this).attr("id");
+    if (parseInt(hourDivEl) === currentHour) {
+        $(this).children("textarea").addClass("present");
+    } else if (parseInt(hourDivEl) > currentHour) {
+        $(this).children("textarea").addClass("future");
+    }
     console.log(hourDivEl);
-    if (currentHour === hourDivEl) {
-        hourDivEl.addClass(".present");
-    } else if (currentHour < hourDivEl) {
-        hourDivEl.addClass(".future");
+});
+
+// function to save user input within textarea
+saveButton.on("click", function() {
+    var userTextInput = $(this).siblings("textarea").val();
+    console.log(userTextInput);
+    var timeSlot = $(this).parent().attr("id");
+    console.log(timeSlot);
+    // var savedToDo = {
+    //     text: userTextInput,
+    //     time: timeSlot
+    // }
+    localStorage.setItem(timeSlot, userTextInput);
+
+});
+
+// // function to pull from local storage
+// function generateSaved () {
+//     for (i = 09; i <= 17; i++) {
+//         $("#" + i).children("textarea").text(localStorage.getItem(i));
+//     }
+// }
+
+$(".time-block").each(function() {
+    
+    if (parseInt($(this).attr("id")) === localStorage.getItem("key")) {
+        $(this).children("textarea").text(localStorage.getItem(value));
     }
 });
 
-// function setColorCode() {
-//     if
-// }
+// generateSaved();
+
+var testButton = $(".saveBtn1");
+
+testButton.on("click", function() {
+    console.log(Object.entries(localStorage));
+});
+
+
 
 // set time interval for current date/time display to increment each second
 setInterval(displayDateAndTime, 1000);
